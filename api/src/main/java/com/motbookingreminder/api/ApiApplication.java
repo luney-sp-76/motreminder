d9aa9125a3b12111014edda942b4a52c4fb918f2
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2024-03-06 07:51:02 +0000

    update Main

--- api/src/main/java/com/motbookingreminder/api/ApiApplication.java
+++ api/src/main/java/com/motbookingreminder/api/ApiApplication.java
@@ -3,6 +3,9 @@ package com.motbookingreminder.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the MOT Booking Reminder API application.
+ */
 @SpringBootApplication
 public class ApiApplication {
 
