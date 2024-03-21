5775cc1c59ef29038003995e4134ca6ed0918265
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2024-03-21 16:22:56 +0000

    update POM

--- src/main/java/com/utilities/sendReminderEmail.java
+++ src/main/java/com/utilities/sendReminderEmail.java
@@ -1,6 +1,8 @@
 package com.utilities;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.services.EmailService;
 
 @Autowired private EmailService emailService;
 
