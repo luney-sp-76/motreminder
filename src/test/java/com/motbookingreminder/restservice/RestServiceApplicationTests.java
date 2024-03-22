1dedc92ce8b64fd82faadd8203bdc6521c86ea8d
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2024-03-22 08:20:43 +0000

    update adds unit test to main

--- src/test/java/com/motbookingreminder/restservice/RestServiceApplicationTests.java
+++ src/test/java/com/motbookingreminder/restservice/RestServiceApplicationTests.java
@@ -1,13 +1,22 @@
 package com.motbookingreminder.restservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RestServiceApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
+
 	}
 
 }
