ee5018fff78bb6695e6b0e83fdb9c5c6c1500ab3
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2024-05-05 15:24:50 +0100

    updates to JS Date Formatter

--- src/main/resources/static/js/formatDateToDDMMYYYY.js
+++ src/main/resources/static/js/formatDateToDDMMYYYY.js
@@ -6,4 +6,15 @@ function formatDateToDDMMYYYY(dateInput) {
             year: 'numeric' 
         }).format(date);
     }
- 
+    
+    function formatDateToDDMMMYYYY(dateInput) {
+        const date = new Date(dateInput);
+        return new Intl.DateTimeFormat('en-GB', {
+            day: '2-digit',
+            month: 'short',  // Changed from '2-digit' to 'short' to get month abbreviation
+            year: 'numeric'
+        }).format(date); // Ensure the formatting includes dashes
+    }
+
+    export { formatDateToDDMMYYYY, formatDateToDDMMMYYYY };
+  
